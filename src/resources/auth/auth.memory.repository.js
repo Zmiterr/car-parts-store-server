b91c01f35cac6b04a78d3738063349aca89c790f
Zmiter <ozmiter@gmail.com>
2021-12-24 13:08:06 +0300

    refactor: make match names

--- src/resources/auth/auth.memory.repository.js
+++ src/resources/auth/auth.memory.repository.js
@@ -1,9 +1,9 @@
 const db = require('../../database/database_connection');
 
-const getAll = async (login, password) =>
-  db.query('SELECT * FROM users WHERE login=$1 AND password=$2', [
+const checkUser = async (login, password) =>
+  db.query('SELECT * FROM USERS WHERE login = $1 AND password = $2', [
     login,
     password,
   ]);
 
-module.exports = { getAll };
+module.exports = { checkUser };
