1ae3350e8a99562f8d0b9020a214984e798e9770
Zmiter <ozmiter@gmail.com>
2022-01-21 21:11:50 +0300

    fear: make token expire like const

--- src/common/config.js
+++ src/common/config.js
@@ -11,4 +11,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   AUTH_MODE: process.env.AUTH_MODE === 'true',
+  TOKEN_EXPIRE: +process.env.TOKEN_EXPIRE,
 };
