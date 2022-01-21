28ec5c4fbaa73b79052859e6124e0c8a0d7ddac2
Zmiter <ozmiter@gmail.com>
2022-01-21 21:35:26 +0300

    feat: return on front omlu required auth user data

--- src/resources/auth/auth.memory.repository.js
+++ src/resources/auth/auth.memory.repository.js
@@ -1,9 +1,9 @@
 const db = require('../../database/database_connection');
 
 const checkUser = async (login, password) =>
-  db.query('SELECT * FROM USERS WHERE login = $1 AND password = $2', [
-    login,
-    password,
-  ]);
+  db.query(
+    'SELECT id, login, role FROM USERS WHERE login = $1 AND password = $2',
+    [login, password]
+  );
 
 module.exports = { checkUser };
